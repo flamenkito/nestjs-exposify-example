c42f4a00248c2523063e57a185075200167370d9
tks2a <flamenkito@users.noreply.github.com>
2026-01-12 12:30:40 +0100

    refactor: parse int pipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { CreateUserDto, UserDto } from './interfaces/user.interface';
 import { UsersService } from './users.service';
 
@@ -12,8 +12,8 @@ export class UsersController {
   }
 
   @Get(':id')
-  getUserById(@Param('id') id: string): UserDto | undefined {
-    return this.usersService.getUserById(parseInt(id));
+  getUserById(@Param('id', ParseIntPipe) id: number): UserDto | undefined {
+    return this.usersService.getUserById(id);
   }
 
   @Post()
