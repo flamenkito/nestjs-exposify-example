beadafbeb6bb25bd887230050d69844321637d46
tks2a <flamenkito@users.noreply.github.com>
2026-01-13 18:17:04 +0100

    refactor: extract css vars

--- apps/api/src/auth/index.ts
+++ apps/api/src/auth/index.ts
@@ -4,7 +4,6 @@ export * from './auth.dto';
 export * from './auth.config';
 export type { AuthResponse, AuthUser, JwtPayload } from '@example/auth';
 export {
-  AuthModule as AuthLibModule,
   JwtAuthGuard,
   Public,
   hasAllPermissions,
