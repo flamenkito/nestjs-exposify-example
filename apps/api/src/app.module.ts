8cd7f471d025d81f97eea41ae19606a4a6574891
Aleksey Sutov <flamenkito@users.noreply.github.com>
2026-01-15 00:08:19 +0100

    feat: default spa

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -7,20 +7,28 @@ import { AuthModule } from './auth';
 import { UsersModule } from './users';
 import { DelayInterceptor } from './delay.interceptor';
 
+const WEB_PREACT_PATH = join(__dirname, '..', '..', 'web-preact');
+const WEB_ANGULAR_PATH = join(__dirname, '..', '..', 'web-angular', 'browser');
+const DEFAULT_SPA_PATH = WEB_ANGULAR_PATH;
+
 @Module({
   imports: [
     AuthModule,
     UsersModule,
     JsonRpcModule,
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', 'web-preact'),
+      rootPath: WEB_PREACT_PATH,
       serveRoot: '/preact',
       exclude: ['/rpc{/*path}'],
     }),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '..', 'web-angular', 'browser'),
+      rootPath: WEB_ANGULAR_PATH,
       serveRoot: '/angular',
       exclude: ['/rpc{/*path}'],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: DEFAULT_SPA_PATH,
+      exclude: ['/rpc{/*path}', '/preact{/*path}', '/angular{/*path}'],
     }),
   ],
   providers: [
